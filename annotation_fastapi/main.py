535d522188571f2b72c6322300fcb401afdf8ecb
bodobraegger <bodo.braegger@gess.ethz.ch>
2025-10-24 18:57:48 +0200

    fix: attempt to import utils before services in main?

--- annotation_fastapi/main.py
+++ annotation_fastapi/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from models import AnnotationRequest, ClusterRequest
+from utils import *
 from services import process_annotation_json, synthesize_guideline_improvements, process_annotation_one_json
 from fastapi.middleware.cors import CORSMiddleware
 import json
